4ef44270b5b7c43c2cfc46d75641c76619b90819
t73liu <t73liu@gmail.com>
2019-07-27 23:06:15 -0700

    feature: Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+import SEO from "../components/seo";
+
+export default () => (
+  <Fragment>
+    <SEO title="Not Found" />
+    <div>Not Found!</div>
+  </Fragment>
+);
