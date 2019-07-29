e14f065fb02b9c4cd160af3fadc2f9c7f5b068d0
t73liu <t73liu@gmail.com>
2019-07-28 17:27:49 -0700

    fix: Fix missing class due to Gatsby issue

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,37 +3,40 @@ import { Link } from "gatsby";
 import "./layout.css";
 
 const Layout = ({ children }) => (
-  <div className="site">
-    <nav
-      className="navbar is-dark"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="navbar-brand">
-        <Link className="navbar-item" to="/">
-          Home
-        </Link>
-        <Link className="navbar-item" to="/blog/">
-          Blog
-        </Link>
-        <Link className="navbar-item" to="/projects/">
-          Projects
-        </Link>
-      </div>
-    </nav>
-    <main className="site-content">{children}</main>
-    <footer className="footer is-fixed-bottom">
-      <div className="content has-text-centered">
-        <p>
-          <strong>Gatsby</strong> starter by{" "}
-          <a href="https://t73liu.github.io">Tao Liu</a>. The{" "}
-          <a href="https://github.com/t73liu/gatsby-engineer-starter">
-            source code
-          </a>{" "}
-          is licensed MIT.
-        </p>
-      </div>
-    </footer>
+  // Extra div required because of https://github.com/gatsbyjs/gatsby/issues/8560
+  <div>
+    <div className="site">
+      <nav
+        className="navbar is-dark"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            Home
+          </Link>
+          <Link className="navbar-item" to="/blog/">
+            Blog
+          </Link>
+          <Link className="navbar-item" to="/projects/">
+            Projects
+          </Link>
+        </div>
+      </nav>
+      <main className="site-content">{children}</main>
+      <footer className="footer is-fixed-bottom">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Gatsby</strong> starter by{" "}
+            <a href="https://t73liu.github.io">Tao Liu</a>. The{" "}
+            <a href="https://github.com/t73liu/gatsby-engineer-starter">
+              source code
+            </a>{" "}
+            is licensed MIT.
+          </p>
+        </div>
+      </footer>
+    </div>
   </div>
 );
 
